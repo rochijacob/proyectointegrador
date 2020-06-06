4e4ce53e4666764e0cd6f2f67fcc30df5b78a0ca
rochijacob <rochijacob@gmail.com>
2020-06-06 16:47:42 -0300

    search3

--- scripts/results.js
+++ scripts/results.js
@@ -7,13 +7,13 @@ console.log(queryStringObj);
 let search = queryStringObj.get('search');
 console.log(search);
 
-
 let proxy = "https://cors-anywhere.herokuapp.com/";
-let url = proxy + 'https://api.deezer.com/search/track?q=' + search; 
-console.log(url);
+let urltracks = proxy + 'https://api.deezer.com/search/track?q=' + search; 
+console.log(urltracks);
+let urlartists = proxy + 'https://api.deezer.com/search/artist?q=' + search; 
+let urlalbums = proxy + 'https://api.deezer.com/search/album?q=' + search;
 
-
-fetch(url)
+fetch(urltracks)
     .then(function(response){
         return response.json();
     })
@@ -25,9 +25,48 @@ fetch(url)
         console.log(resultados);
         
         resultados.forEach(resultado => {
-            lista.innerHTML += '<li>' + resultado.name + '</li>'
+            lista.innerHTML += '<li>' + resultado.title + '</li>'
         });
     })
     .catch(function(error){
         console.log(error)
     })
+
+    fetch(urlartists)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        
+        let lista = document.querySelector('.lista')
+        let resultados = datos.data;
+        console.log(resultados);
+        
+        resultados.forEach(resultado => {
+            lista.innerHTML += '<li>' + '<img src="' +  resultado.picture  + '"></img>' + resultado.name + '</li>';
+        });
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
+    fetch(urlalbums)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        
+        let lista = document.querySelector('.lista')
+        let resultados = datos.data;
+        console.log(resultados);
+        
+        resultados.forEach(resultado => {
+            lista.innerHTML += '<li>' + '<img src="' +  resultado.cover  + '"></img>' + resultado.title + '</li>';
+        });
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
